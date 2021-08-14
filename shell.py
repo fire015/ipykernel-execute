e60ce38a7b4198f6c31f40f1836cb29e250e6cb4
fire015 <emailfire@gmail.com>
2021-08-14 13:53:08 +0100

    Initial commit

---
+++ shell.py
@@ -0,0 +1,72 @@
+# pip install ipykernel
+
+from jupyter_client import manager
+from subprocess import STDOUT
+from time import time
+
+STARTUP_TIMEOUT = 60
+TIMEOUT = 60
+
+
+def start_new_kernel(**kwargs):
+    #kwargs['stderr'] = STDOUT
+    #kwargs['stdout'] = STDOUT
+
+    return manager.start_new_kernel(startup_timeout=STARTUP_TIMEOUT, **kwargs)
+
+def get_reply(kc, msg_id, timeout=TIMEOUT, channel='shell'):
+    t0 = time()
+    while True:
+        get_msg = getattr(kc, f'get_{channel}_msg')
+        reply = get_msg(timeout=timeout)
+        if reply['parent_header']['msg_id'] == msg_id:
+            break
+        # Allow debugging ignored replies
+        print(f"Ignoring reply not to {msg_id}: {reply}")
+        t1 = time()
+        timeout -= t1 - t0
+        t0 = t1
+    return reply
+
+def assemble_output(get_msg):
+    """assemble stdout/err from an execution"""
+    stdout = ''
+    stderr = ''
+    while True:
+        msg = get_msg(timeout=1)
+        msg_type = msg['msg_type']
+        content = msg['content']
+        if msg_type == 'status' and content['execution_state'] == 'idle':
+            # idle message signals end of output
+            break
+        elif msg['msg_type'] == 'stream':
+            if content['name'] == 'stdout':
+                stdout += content['text']
+            elif content['name'] == 'stderr':
+                stderr += content['text']
+            else:
+                raise KeyError("bad stream: %r" % content['name'])
+        else:
+            # other output, ignored
+            pass
+    return stdout, stderr
+
+def execute(code='', kc=None, **kwargs):
+    msg_id = kc.execute(code=code, **kwargs)
+    reply = get_reply(kc, msg_id, TIMEOUT)
+
+    return msg_id, reply['content']
+
+
+
+
+with open('test.py') as f:
+    code = f.read()
+
+km, kc = start_new_kernel()
+msg_id, content = execute(code=code, kc=kc)
+stdout, stderr = assemble_output(kc.get_iopub_msg)
+print(content)
+print(stdout)
+kc.stop_channels()
+km.shutdown_kernel(now=True)
